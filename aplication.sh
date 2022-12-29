a160451a3cf713b282d51d51ae35cf24df974769
João V. de Souza (Will) <61891017+JoaoVWill@users.noreply.github.com>
2022-12-29 11:41:11 -0300

    att

--- aplication.sh
+++ aplication.sh
@@ -24,7 +24,7 @@ echo "Vamos expor a aplicação na porta 80 agora"
 echo "kubectl expose deployment nginx --type=NodePort --port=80 -n $namespace"
 kubectl expose deployment nginx --type=NodePort --port=80 -n $namespace
 echo "Agora o IP da aplicação para acessar o site default do NGINX"
-echo "kubectl get pod -o wide -n $namespace"
-kubectl get pod -o wide -n $namespace
+#echo "kubectl get pod -o wide -n $namespace"
+#kubectl get pod -o wide -n $namespace
 
 echo "FIM DO SCRIPT, bye bye"
