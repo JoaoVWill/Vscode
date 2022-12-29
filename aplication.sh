8ce4b675915dd8024597c36dc5f5946743047657
João V. de Souza (Will) <61891017+JoaoVWill@users.noreply.github.com>
2022-12-29 13:36:16 -0300

    Att - Export IP Externo

--- aplication.sh
+++ aplication.sh
@@ -16,30 +16,24 @@ kubectl create namespace $namespace
 echo "Depois de criar o Namespace $namespace vamos criar nossa aplicação, no caso um NGINX"
 echo "kubectl create deployment nginx --image=nginx:1.23.1 -n $namespace"
 kubectl create deployment nginx --image=nginx:1.23.1 -n $namespace
-read
-
-clear
 
 echo "Vamos expor a aplicação na porta 80 agora"
 echo "kubectl expose deployment nginx --type=NodePort --port=80 -n $namespace"
 kubectl expose deployment nginx --type=NodePort --port=80 -n $namespace
-echo "Agora o IP da aplicação para acessar o site default do NGINX"
-sudo apt-get update -y
-sudo apt-get install nginx -y
-
+echo "Agora gerar o IP da aplicação para acessar o site default do NGINX de maneira externa"
 sudo unlink /etc/nginx/sites-enabled/default
 
-cd etc/nginx/sites-available/
+cd /etc/nginx/sites-available/
 
 NODE_IP=$(kubectl get nodes -o wide | grep -i ready | awk '{print $6}');
 NODE_PORT=$(kubectl get svc nginx -o yaml -n $namespace| grep -i nodeport | awk '{print $3}');
 
-sudo echo 'server {' > reverse-proxy.conf
-sudo echo '    listen 80;' >> reverse-proxy.conf
-sudo echo '    location / {' >> reverse-proxy.conf
-sudo echo '        proxy_pass http://'$NODE_IP':'$NODE_PORT';' >> reverse-proxy.conf
-sudo echo '    }' >> reverse-proxy.conf
-sudo echo '}' >> reverse-proxy.conf
+sudo bash -c "echo 'server {' > reverse-proxy.conf"
+sudo bash -c "echo '    listen 80;' >> reverse-proxy.conf"
+sudo bash -c "echo '    location / {' >> reverse-proxy.conf"
+sudo bash -c "echo '        proxy_pass http://'$NODE_IP':'$NODE_PORT';' >> reverse-proxy.conf"
+sudo bash -c "echo '    }' >> reverse-proxy.conf"
+sudo bash -c "echo '}' >> reverse-proxy.conf"
 
 sudo ln -s /etc/nginx/sites-available/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
 
